a5caf84af74b1484512598fc15685e42c631c48d
JP <john5onpham79@gmail.com>
2022-03-06 16:56:29 -0600

    change text

--- tamu-nav/App.js
+++ tamu-nav/App.js
@@ -4,11 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>tamu nav!</Text>
+      <Text>Main Page</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
